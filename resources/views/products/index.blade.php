ea3ccabd895421c60b2327aaeb08571d3652afb9
Jure Sabljo <juresabljo@MacBook-Pro-od-Jure.local>
2024-03-29 18:34:17 +0100

    Updated index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>All products:</h1>
+    <h1>Svi proizvodi:</h1>
     <div class="container-fluid">
         <div class="row">
             <div class="col-4">
